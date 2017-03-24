8ad702dd68a155a9feac5b51ca534be9ac2ce178
fabiolamfleury <fabiola.mfleury@gmail.com>
2017-03-24 13:15:09 -0300

    Fixed out message on q03 and deleted
    unnecessary files

--- q03/ordenate.c
+++ q03/ordenate.c
@@ -4,9 +4,12 @@
 //Print the number in descending order
 void DescendingOrder(int * number_vector, int vector_size) {
   int counter;
-
-  for(counter = vector_size -1; counter >= 0; counter--)
+  if(vector_size > 0){
+    printf("Saida decrescente: \n");
+  }
+  for(counter = vector_size -1; counter >= 0; counter--){
     printf("%d ", number_vector[counter]);
+  }
 
   printf("\n");
 }
@@ -15,6 +18,9 @@ void DescendingOrder(int * number_vector, int vector_size) {
 void AscendingOrder(int * number_vector, int vector_size){
   int counter;
 
+  if(vector_size > 0){
+    printf("Saida crescente: \n");
+  }
   for(counter = 0; counter < vector_size; counter++)
     printf("%d ", number_vector[counter]);
 
