4f1285ed19f7a45739bd5ec367ece8bfe8fe0005
fabiolamfleury <fabiola.mfleury@gmail.com>
2017-05-26 18:05:20 -0300

    Updating solution to trabalho04

--- trabalho04/b/libseno.c
+++ trabalho04/b/libseno.c
@@ -1,18 +1,17 @@
-#include <math.h>
 /*calcula o seno com a aproxima da serie sen(x)=x - x³/3! + x⁵/5! - x⁷/7!*/
 double seno(double angulo){
-  double elemento2 = (pow (angulo, 3) / 6); /* angulo^3 / 3!*/
-  double elemento3 = (pow (angulo, 5) / 120); /*angulo^5 / 5!*/
-  double elemento4 = (pow (angulo, 7) / 5040); /*angulo^7 / 7!*/
+  double elemento2 = (angulo * angulo * angulo) / 6); /* angulo^3 / 3!*/
+  double elemento3 = (angulo * angulo * angulo * angulo * angulo / 120); /*angulo^5 / 5!*/
+  double elemento4 = (angulo * angulo * angulo * angulo * angulo * angulo * angulo/ 5040); /*angulo^7 / 7!*/
 
   double seno = angulo - elemento2 + elemento3 - elemento4;
   return seno;
 }
 
 double arc_seno(double seno){
-  double elemento2 = (pow (seno, 3) / 6); /* angulo^3 / 6*/
-  double elemento3 = 3 * (pow (seno, 5) / 40); /*angulo^5 / 40*/
-  double elemento4 = 5 * (pow (seno, 7) / 15);
+  double elemento2 = (angulo * angulo * angulo) / 6); /* angulo^3 / 3!*/
+  double elemento3 = (angulo * angulo * angulo * angulo * angulo / 120); /*angulo^5 / 5!*/
+  double elemento4 = (angulo * angulo * angulo * angulo * angulo * angulo * angulo/ 5040); /*angulo^7 / 7!*/
 
   double arcseno = seno + elemento2 + elemento3 + elemento4;
   return arcseno;
---
+++ trabalho04/c/main.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <dlfcn.h>
+
+int main(int argc, char const *argv[]) {
+  void* handle = dlopen("libseno.so",RTLD_LAZY);
+  if(!handle)
+  {
+    fprintf(stderr, "%s\n", dlerror());
+    exit(1);
+  }
+  char *error;
+
+  if(argc > 2){
+    double numero = 0;
+    sscanf(argv[2], "%lf", &numero);
+
+    if (argv[1][1] == 's') {
+      double (*resposta)(double) = dlsym (handle, "seno");
+        if((error = dlerror()) != NULL)
+        {
+          fprintf(stderr,"%s\n", error);
+          exit(1);
+        } else {
+          printf("\nseno(%s) = %lf\n", argv[2], (resposta*)(numero));
+          dlclose(handle);
+        }
+
+    } else if (argv[1][1] == 'a') {
+        double (*resposta2)(double) = dlsym (handle, "arc_seno");
+        if((error = dlerror()) != NULL)
+        {
+          fprintf(stderr,"%s\n", error);
+          exit(1);
+        } else {
+          double resultado = (resposta2*)(numero);
+          printf("\narcseno(%s) = %lf\n", argv[2],resultado);
+          dlclose(handle);
+        }
+    } else {
+      printf("Você deve digitar s ou a seguido de um numero");
+    }
+  }
+  else{
+    printf("Você deve especificar se quer calcular seno ou arcseno, e o valor de entrada.");
+  }
+  return 0;
+}
