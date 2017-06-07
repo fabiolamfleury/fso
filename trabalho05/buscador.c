a1e6b857011a48fb1ac4ea4abfe01c54dc4cfaea
fabiolamfleury <fabiola.mfleury@gmail.com>
2017-06-07 16:57:58 -0300

    Studying functions to be used in solution of program

---
+++ trabalho05/buscador.c
@@ -0,0 +1,53 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+
+int main(int argc, char const *argv[]) {
+  if (argc == 4){
+  }
+  else{
+    printf("A quantidade de argumentos passados deve ser três.");
+  }
+  printf("%d",argc);
+  return 0;
+}
+
+
+void printdir(char *dir, int depth)
+{
+    DIR *dp;
+    struct dirent *entry;
+    struct stat statbuf;
+    if((dp = opendir(dir)) == NULL) {
+        fprintf(stderr,"cannot open directory: %s\n", dir);
+        return;
+    }
+    chdir(dir);
+    while((entry = readdir(dp)) != NULL) {
+        lstat(entry->d_name,&statbuf);
+        if(S_ISDIR(statbuf.st_mode)) {
+            /* Found a directory, but ignore . and .. */
+            if(strcmp(".",entry->d_name) == 0 ||
+                strcmp("..",entry->d_name) == 0)
+                continue;
+            printf("%*s%s/\n",depth,"",entry->d_name);
+            /* Recurse at a new indent level */
+            printdir(entry->d_name,depth+4);
+        }
+        else printf("%*s%s\n",depth,"",entry->d_name);
+    }
+    chdir("..");
+    closedir(dp);
+}
+
+int main()
+{
+    printf("Directory scan of /home:\n");
+    printdir("/home",0);
+    printf("done.\n");
+    exit(0);
+}
